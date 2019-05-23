797c05799296170d324907c8ed5e007c9d0c9c60
Ümit Öztürk <1150606004@nku.edu.tr>
2019-05-23 17:37:32 +0300

    Added Article Struct and Pass parameters Handle Request Function

--- ex004-mux/main.go
+++ ex004-mux/main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request)  {
+	articles := Articles{
+		Article{Title:"Test Title", Desc: "Test Description", Content: "Test Content"},
+	}
+
+	fmt.Println("Endpoint: Home All Articles")
+	json.NewEncoder(w).Encode(articles)
+	
+}
 
 func homePage(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprint(w, "Home Endpoint Hit.")
@@ -12,6 +31,7 @@ func homePage(w http.ResponseWriter, r *http.Request)  {
 
 func handleRequest()  {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8001", nil))
 }
 
