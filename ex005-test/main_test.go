ee4a8525bef721caf1dbc5f0f8012f764a9dedbd
Ümit Öztürk <1150606004@nku.edu.tr>
2019-05-26 18:15:21 +0300

    Go Test with Table Inputted

--- ex005-test/main_test.go
+++ ex005-test/main_test.go
@@ -9,3 +9,22 @@ func TestCalculate(t *testing.T)  {
 		t.Error("Expected 2 + 2 to equal 4.")
 	}
 }
+
+func TestTableCalculate(t *testing.T)  {
+	var tests = []struct{
+		input int
+		expected int
+	}{
+		{2, 4},
+		{-1, 1},
+		{0, 2},
+		{9999, 10001},
+	}
+
+	for _, test := range tests {
+		if output := Calculate(test.input); output != test.expected{
+			t.Error("Test Failed: {} inputted, {} expected, received {}", test.input, test.expected, output)
+		}
+	}
+	
+}
