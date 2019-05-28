32d052f635108143a7e56a2ab8e9b2baa760fd07
Ümit Öztürk <1150606004@nku.edu.tr>
2019-05-28 18:46:40 +0300

    Select Example in Mysql with Go

--- ex006-mysql/main.go
+++ ex006-mysql/main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
 )
+
+type User struct {
+	Name string
+}
 
 func main()  {
 	fmt.Println("MySQL Example with Go.")
 
 	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/testdb")
-
 	if err != nil{
 		panic(err.Error())
 	}
 
 	defer db.Close()
-	
+
+	results, err := db.Query("SELECT name FROM users")
+	if err != nil {
+		panic(err.Error())
+	}
+	for results.Next(){
+		var user User
+
+		err = results.Scan(&user.Name)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(user.Name)
+	}
+
 	fmt.Println("Successfully Connected to Database.")
 
 }
