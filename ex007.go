04e1cb3695fcb056239ec4411ee1eb49763f4edc
Ümit Öztürk <1150606004@nku.edu.tr>
2019-06-01 16:21:37 +0300

    Pointer Receivers Example

--- ex007.go
+++ ex007.go
@@ -19,8 +19,29 @@ func (c car) kmh() float64 {
 	
 }
 
+func (c car) mph() float64 {
+	return float64(c.gasPedal) * (c.topSpeedKmh/uSixTeenBitMax/kmhMultiple)
+
+}
+
+func (c *car) newTopSpeed(newSpeed float64) float64 {
+	c.topSpeedKmh = newSpeed
+	return float64(c.gasPedal) * (c.topSpeedKmh/uSixTeenBitMax/kmhMultiple)
+
+}
+
+func newerTopSpeed(c car, speed float64) car {
+	c.topSpeedKmh = speed
+	return c
+}
+
 func main() {
 	instCar := car{gasPedal: 20000, breakePedal: 0, steeringPedal: 150, topSpeedKmh: 200}
 	fmt.Println(instCar.gasPedal)
 	fmt.Println(instCar.kmh())
+	fmt.Println(instCar.mph())
+	instCar.newTopSpeed(500)
+	instCar = newerTopSpeed(instCar, 100)
+	fmt.Println(instCar.kmh())
+	fmt.Println(instCar.mph())
 }
